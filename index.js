58d795bd5013d9ab6c68d82e7d36cee45143ac18
harsh-kanjar <harshlankar07@gmail.com>
2024-08-31 13:03:34 +0530

    update index

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ connectToMongo();
 // -------------------------EXPRESS SERVER------------------------------
 const app = express();
 const port = 5000;
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173', // Allow specific origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers
+}));
+
 
 // -------------NECESSARY TO USE req.body (Middleware)------
 app.use(express.json())
