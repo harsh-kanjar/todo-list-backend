addab2af787c4122da838db03e54286f8232f23f
harsh-kanjar <harshlankar07@gmail.com>
2024-08-31 12:37:39 +0530

    update db.js and vercel.json

--- db.js
+++ db.js
@@ -8,7 +8,8 @@ const PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME || 'todosDB';
 
 const Connection = () => {
-    const MONGODB_URI = `mongodb://localhost:27017/Mytodos`;
+    // const MONGODB_URI = `mongodb://localhost:27017/Mytodos`;
+    const MONGODB_URI = 'mongodb+srv://harshkanjar007:pzUWMWuAwGB7Zc6q@cluster0.qzcka.mongodb.net/'
 
     mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('Database connected successfully'))
